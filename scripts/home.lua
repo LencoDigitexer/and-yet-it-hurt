2413650432db03e9709762a59a4a82004f59e6ec
LencoDigitexer <addminuser258@gmail.com>
2020-02-15 17:40:55 +0300

    Перевод локации 'Дом'

--- scripts/home.lua
+++ scripts/home.lua
@@ -14,16 +14,16 @@ function Home:init()
 		self:setText("")
 	else
 		Events.hasBeenAtHomeBefore = true
-		self:setText("[username]'s parents were tailors. Everyone in town had their clothes made by them. Today all the sheep were brought inside to be sheared by [username]'s father, while [his] mother was sewing new clothes.")
+		self:setText("Родители [username] были портными. У каждого в городе была своя одежда, сшитая ими. Сегодня все овцы были приведены в дом, чтобы их стриг отец [username], в то время как её мать шила новую одежду.")
 	end
 
 	self:setOptions({
 		{
-			text = "Talk to your mom.",
+			text = "Поговорить с мамой.",
 			func = F(self, "mom")
 		},
 		{
-			text = "Talk to your dad.",
+			text = "Поговорить с папой.",
 			func = F(self, "dad")
 		}
 	})
@@ -34,48 +34,48 @@ function Home:mom()
 	self:loadArt("mom")
 
 	if Events.gotPantsFromMom then
-		self:setText([["Hi, sweetie. Did you give Edbur his pants?" said [username]'s mother.]])
+		self:setText([["Привет, солнышко. Ты отдала Эдбуру его штаны?" сказала мама [username].]])
 		self:setOptions({
 			{
-				text = [["I did."]],
+				text = [["Да, сделано"]],
 				condition = function () print(Events.gavePantsToEdbur) return Events.gavePantsToEdbur end,
-				response = [["Thanks, sweety, could you hand me the-" Suddenly [username]'s mother was interrupted by a loud bell. [His] mother's smile instantly vanished. "It can't be! What do we do, Harold?". [username] was confused and asked what was going on.]],
+				response = [["Спасибо, солнышко, передай, пожалуйста, мне... -" Внезапно речь мамы [username] была прервана громким звонком. Улыбка матери мгновенно исчезла. "Этого не может быть! Что нам делать, Гарольд?". [username] смутилась и спросила, что происходит.]],
 				func = function () Game:removeFile("house") Game:removeFile("weapon shop") end,
 				options = {
 					{
-						text = "Continue.",
+						text = "Продолжить.",
 						func = F(self, "event", 1)
 					}
 				}
 			},
 			{
-				text = [["I did not."]],
-				response = [["Well please hurry, Edbur's legs are probably getting cold."]],
+				text = [["Я не..."]],
+				response = [["Ну, пожалуйста, поторопись, у Эдбура, наверное, ноги мерзнут."]],
 				options = {
 					{
-						text = "Back.",
+						text = "Назад.",
 						func = F(self, "init")
 					}
 				}
 			}	
 		})
 	else
-		self:setText([["Hi, sweetie. Can you bring these pants to Edbur?", asked [username]'s mother]])
+		self:setText([["Привет, милая. Ты можешь отнести эти штаны Эдбуру?", спросила мама [username] ]])
 		self:setOptions({
 			{
-				text = [["Okay."]],
-				response = [["Okay," said [username] with a clear lack of enthusiasm in [his] voice. [His] mother thanked [him] and handed [him] Edbur's pants.]],
+				text = [["Хорошо."]],
+				response = [["Хорошо," сказала [username] с явным отсутствием энтузиазма в её голосе. Её мать поблагодарила её и вручила ей штаны Эдбура.]],
 				item = "pantsEdbur",
 				event = "gotPantsFromMom",
 				options = {
 					{
-						text = "Back.",
+						text = "Назад.",
 						func = F(self, "init")
 					}
 				}
 			},
 			{
-				text = [["Not now."]],
+				text = [["Не сейчас."]],
 				func = F(self, "init")
 			}	
 		})
@@ -87,57 +87,57 @@ function Home:dad()
 	self:loadArt("dad")
 
 	if Events.goldTakenByDad then
-		self:setText([["I hate lunch without milk," said [username]'s father.]])
+		self:setText([["Я ненавижу обедать без бокала молока," сказал папа [username].]])
 		self:setOptions({
 			{
-				text = [[Back.]],
+				text = [[Назад.]],
 				func = F(self, "init")
 			}
 		})
 	elseif Events.gotGoldFromDad then
-		self:setText([["Hello [username]," said [username]'s father, who was still shearing the sheep. "Did you get the milk?"]])
+		self:setText([["Привет [username]," сказал папа [username], который всё ещё стриг овец. "Ты нашла молоко?"]])
 		self:setOptions({
 			{
-				text = [["The store is closed today."]],
+				text = [["Магазин закрыт сегодня."]],
 				condition = function () return Events.sawStore end,
-				response = [["It is? Darn." said [username]'s father as he took the 5 gold from [username]. "No milk with today's lunch then."]],
+				response = [["Вот как? Чёрт." выругался папа [username] забирая у нее 5 золотых монет. "Тогда никакого молока на обед."]],
 				gold = -5,
 				event = "goldTakenByDad",
 				options = {
 					{
-						text = "Back.",
+						text = "Назад.",
 						func = F(self, "init")
 					}
 				}
 			},
 			{
-				text = [["Not yet."]],
-				response = [["Well hurry up," said [username]'s father, "or else we'll have lunch without milk."]],
+				text = [["Еще нет."]],
+				response = [["Тогда чего ждёшь?" спросил папа [username], "Хочешь чтобы мы обедали без молока?."]],
 				options = {
 					{
-						text = "Back.",
+						text = "Назад.",
 						func = F(self, "init")
 					}
 				}
 			}	
 		})
 	else
-		self:setText([["Hello [username]," said [username]'s father, who was busy shearing the sheep. "Can you go to the store and buy some milk?"]])
+		self:setText([["Приветствую, [username]," сказал папа [username], который всё ещё стриг овец. "Можешь сходить в магазин за молоком?"]])
 		self:setOptions({
 			{
-				text = [["Okay."]],
-				response = [["Okay.", agreed [username] reluctantly. [His] father thanked [him] and gave [him] 5 gold for the milk.]],
+				text = [["Хорошо."]],
+				response = [["Хорошо.", неохотно согласилась [username]. Её отец поблагодарил её и дал 5 золотых монет на молоко]],
 				gold = 5,
 				event = "gotGoldFromDad",
 				options = {
 					{
-						text = "Back.",
+						text = "Назад.",
 						func = F(self, "init")
 					}
 				}
 			},
 			{
-				text = [["Not now."]],
+				text = [["Не сейчас."]],
 				func = F(self, "init")
 			}
 		})
@@ -150,28 +150,28 @@ function Home:event(i)
 
 	self:setOptions({
 		{
-			text = "Next.",
+			text = "Дальше.",
 			func = F(self, "event", i + 1)
 		}
 	})
 
 	if i == 1 then
-		self:setText([["Calm down," said [username]'s father. "If we stay inside and keep quiet we should be safe." [username] once again asked what was going on.]])
+		self:setText([["Успокойся," сказал папа [username]. " Если мы останемся внутри и будем сидеть тихо, мы будем в безопасности." [username] ещё раз спросила, что происходит.]])
 	elseif i == 2 then
-		self:setText([["Quiet!" said [username]'s father in a fierce but whispering tone. [username], still confused, didn't dare to speak one more word.]])
+		self:setText([["Тихо!" яростно, но шепотом сказал отец [username], всё ещё растерянный, он не осмеливался сказать больше ни слова.]])
 	elseif i == 3 then
 		-- self.visible = false
-		self:setText([[Suddenly the ground started shaking, making the sheep start bleating hysterically.]])
+		self:setText([[Внезапно земля задрожала, и овцы истерически заблеяли.]])
 	elseif i == 4 then
 		-- self.visible = true
-		self:setText([["Shit!" said [username]'s father, not whispering anymore. "Help me calm them down, if they are too loud the dra-"]])
+		self:setText([["БЛЯТЬ!" крикнул папа [username]. "Помогите мне их успокоить - они слишком громкие!"]])
 	elseif i == 5 then
 		self.visible = false
 		Art.new(self, "the_end_qm")
-		self:setText([[The next instance, [username] was surrounded by flames. The heat was extreme, and [he] screamed in pain as [he] [was] burned alive. It was the end of [him].]])
+		self:setText([[В следующий раз, [username] Была окружена пламенем. Жара была невыносимой, и она кричала от боли, когда она была сожжена заживо. Это был её конец.]])
 		self:setOptions({
 			{
-				text = "Restart.",
+				text = "Перезапустить.",
 				func = F(self, "restart")
 			}
 		})
