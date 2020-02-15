a0051f0cfed7d93ba2cbae28bd45b6d5ca635524
LencoDigitexer <addminuser258@gmail.com>
2020-02-15 13:56:33 +0300

    Перевод главного скрипта

--- scripts/readme.lua
+++ scripts/readme.lua
@@ -10,24 +10,24 @@ function ReadMe:new()
     end
 
     if SAVE.exists then
-        self:setText([[Hey, welcome back. I see you've played this game before. Do you want to continue from where you left off?]])
+        self:setText([[Эй, добро пожаловать сново. Я вижу, ты уже играл в эту игру раньше. Хотите продолжить с того места, на котором остановились?]])
         self:setOptions({
             {
-                text = "Yes, load my game.",
+                text = "Да, загрузи сохранение.",
                 func = F(self, "load")
             },
             {
-                text = "No, start a new game.",
+                text = "Нет, давай начнем сначала.",
                 func = F(self, "tips")
             },
         })
     else
-        self:setText([[How to play: At the bottom you will see the options. You can select an option by filling the [] with one or multiple characters.
-For example: [s] - I understand how options work.
-After filling your selection you can save the file with Ctrl + S. Try it out!]])
+        self:setText([[Как играть: внизу вы увидите варианты. Вы можете выбрать опцию, заполнив [] одним или несколькими символами. 
+Например: [s] - Я понимаю, как работают опции. 
+После заполнения вашего выбора вы можете сохранить файл с помощью Ctrl + S. попробуйте!]])
         self:setOptions({
             {
-                text = "I understand how to select an option.",
+                text = "Я понимаю, как выбрать тот или иной вариант.",
                 func = F(self, "tips")
             }
         })
@@ -36,20 +36,20 @@ end
 
 
 function ReadMe:tips()
-    self:setText([[Good! Before we start the game I have some tips for you.
-With Windows key + Left/Right arrow key you can put the windows nicely next to each other.
-And did you know you can scroll in and out with Ctrl + Scroll wheel?]])
+    self:setText([[Хорошо! Прежде чем мы начнем игру, у меня есть несколько советов для вас.
+С помощью клавиши Windows + клавиши со стрелками влево/вправо вы можете поместить окна красиво рядом друг с другом.
+И знаете ли вы, что вы можете изменять масштаб с помощью Ctrl + колесо прокрутки?]])
     self:setOptions({
         {
-            text = "Thank you for the tips.",
+            text = "Спасибо за советы.",
             func = F(self, "continue", 1)
         },
         {
-            text = "I did not know that!",
+            text = "Я этого не знал!",
             func = F(self, "continue", 2)
         },
         {
-            text = "I already knew that!",
+            text = "Я уже знал это!",
             func = F(self, "continue", 3)
         }
     })
@@ -58,30 +58,30 @@ end
 function ReadMe:continue(n)
     local start = ""
     if n == 1 then
-        start = "You're welcome!"
+        start = "Всегда пожалуйста!"
     elseif n == 2 then
-        start = "Well now you do know!"
+        start = "Ну вот теперь ты знаешь!"
     elseif n == 3 then
-        start = "Very good!"
+        start = "Очень хорошо!"
     end
 
-    self:setText(start .. [[ You are almost ready to go on an adventure. Almost.
-First, fill the brackets ([])  with the name of your character. The name can't be longer than 15 characters.
-For example: [William] or [Elizabeth] -  That is the name of my character.]])
+    self:setText(start .. [[ Вы почти готовы отправиться в приключение. Почти.
+Во-первых, заполните скобки ([]) именем вашего персонажа. Имя не может быть длиннее 15 символов.
+Например: [Иван] или [Мария] - так зовут моего персонажа.]])
 
     if n == 4 then
-        self:setText([[You changed your mind on the name? That's okay.
-Fill the brackets ([]) with the name of your character. The name can't be longer than 15 characters.
-For example: [William] or [Elizabeth] -  That is the name of my character.]])
+        self:setText([[Вы передумали насчет названия? Это нормально.
+Заполните скобки ([]) именем вашего персонажа. Имя не может быть длиннее 15 символов.
+Например: [Иван] или [Мария] - так зовут моего персонажа.]])
     end
 
 	self:setOptions({
         {
-            text = "That is the name of my character.",
+            text = "Это имя моего персонажа",
             func = F(self, "setName")
         },
         {
-            text = "I can't decide on a name.",
+            text = "Я никак не могу определиться с именем.",
             func = F(self, "setName", true)
         }
     })
@@ -91,19 +91,19 @@ function ReadMe:setName(pick)
     print(pick)
     local name
     if pick then
-        local names = {"Elizabeth", "Isabella", "Margaret", "Johnathon", "Daniel", "Thaeson"}
+        local names = {"Арахна", "Лилия", "Бэтти", "Орфей", "Миднайт", "Анубис"}
         name = lume.randomchoice(names)
-        self:setText("Okay, then I'll choose... " .. name .. "! Now, how should we talk about this character?")
+        self:setText("Ладно, тогда я сам выберу... " .. name .. "! Теперь, как мы должны говорить об этом персонаже?")
     else
-        local match = self.rContent:sub(1000, #self.rContent):match("%[([^%]]+)%]%s%-%s" .. Utils.litString("That is the name of my character"))
+        local match = self.rContent:sub(1000, #self.rContent):match("%[([^%]]+)%]%s%-%s" .. Utils.litString("Это имя моего персонажа."))
 
         if match then
             if #match > 15 then
-                self:setText("This name is too long! Please don't use more than 15 characters.")
+                self:setText("Это имя слишком длинное! Пожалуйста, не используйте более 15 символов.")
                 return
             end
-            self:setText([[So your character's name is ]] .. match .. [[? I like it!
-How should we talk about this character?]])
+            self:setText([[Итак, имя вашего персонажа ]] .. match .. [[? Мне нравится это!
+Как мы должны говорить об этом персонаже?]])
             name = match
         else
             return
@@ -126,7 +126,7 @@ How should we talk about this character?]])
             func = F(self, "pronouns", "they")
         },
         {
-            text = "I want to change the name.",
+            text = "Я хочу сменить имя.",
             func = F(self, "continue", 4)
         }
     })
@@ -135,9 +135,9 @@ end
 function ReadMe:pronouns(p)
     Player.pronoun = p
 
-    self:setText([[It is done. Now, let us begin.
-And yet it hurt. A story about [username], a kid from Eastown who lived a happy and peaceful life with [his] parents.
-(Open one of the files in the folder.)]])
+    self:setText([[Ура. Давай начнем.И все же было больно. 
+Эта история о [username], ребёнке из Восточного Города, который жил счастливой и мирной жизнью с его родителями.
+(Откройте один из файлов в папке.)]])
     self:setOptions({})
 
     self.deleteOnClose = true
@@ -168,7 +168,7 @@ function ReadMe:load()
             w = 1
         end
     end
-    self:setText("Very well. Let's continue the adventure of " .. Player.name .. ".")
+    self:setText("Очень хорошо. Давайте продолжим наше приключение" .. Player.name .. ".")
     self:setOptions({})
     self.deleteOnClose = true
     Game:addFile(require("edbur_post_lament")(nil, 9))
