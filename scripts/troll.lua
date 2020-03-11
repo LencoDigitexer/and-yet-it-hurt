e69036664484b1c13f4161d1a8cf810470e1f759
LencoDigitexer <addminuser258@gmail.com>
2020-03-11 16:52:19 +0200

    troll Встсреча с троллем

--- scripts/troll.lua
+++ scripts/troll.lua
@@ -8,20 +8,20 @@ function Troll:new()
     self.anim:add("none", 1)
 	self.anim:add("not", 2)
 
-    self:setText([[As [username] walked up the hill, [he] encountered a troll blocking the bridge. "You may NOT pass!" said the troll.]])
+    self:setText([[Когда [username] шла вверх по холму, она столкнулась с троллем, блокирующим мост. "Вы НЕ МОЖЕТЕ пройти!" - сказал тролль.]])
     self:setOptions({
         {
-            text = [["May I pass?"]],
+            text = [["Могу я пройти?"]],
             func = F(self, "ask"),
             default = true
         },
         {
-            text = [["Why not?"]],
+            text = [["А почему бы и нет?"]],
             response = "Because that is what I said!",
             remove = true
         },
         {
-            text = "Back.",
+            text = "Назад.",
             func = F(self, "back")
         }
     })
@@ -30,34 +30,34 @@ end
 
 function Troll:ask()
     self.rContent = love.filesystem.read(self.file)
-    if self.rContent:find("   |     | # /``-.    | You may pass!", 1, true) or self.rContent:find("   |     | # /``-.    | You may  pass!", 1, true) then
-        self:setText([["Did you not hear what I said? You may pass! Wait, no that's not what I meant to say!"]])
+    if self.rContent:find("   |     | # /``-.    | Вы можете пройти!", 1, true) or self.rContent:find("   |     | # /``-.    | Вы можете  пройти!", 1, true) then
+        self:setText([["Разве ты не слышал, что я сказал? Вы можете пройти! Подожди, нет, я не это хотел сказать!"]])
         self.anim:set("may")
         self:setOptions({
             {
-                text = [["Thanks!"]],
-                response = [["Thanks!" said [username], and [he] crossed the bridge.]],
+                text = [["Спасибо!"]],
+                response = [["Спасибо!" сказала [username], и она пересекла мост.]],
                 options = {
                     {
-                        text = "Continue walking.",
+                        text = "Продолжать идти.",
                         func = F(self, "cross")
                     }
                 }
             }
         })
     else
-        self:setText([["Did you not hear what I said? You may NOT pass!"]])
+        self:setText([["Разве ты не слышал, что я сказал? Вы не можете пройти!"]])
         self:setOptions({
             {
-                text = [["May I pass?"]],
+                text = [["Могу я пройти?"]],
                 func = F(self, "ask")
             },
             {
-                text = [["Why not?"]],
+                text = [["А почему бы и нет?"]],
                 response = "Because that is what I said!"
             },
             {
-                text = "Back."
+                text = "Назад."
             }
         })
     end
@@ -70,7 +70,7 @@ function Troll:cross()
 end
 
 function Troll:back()
-    self:setText("[username] decided to walk back.")
+    self:setText("[username] решила вернуться пешком.")
 	self.deleteOnClose = true
 	self:setOptions({})
 	Game:addFile(require("westown_gate")())
