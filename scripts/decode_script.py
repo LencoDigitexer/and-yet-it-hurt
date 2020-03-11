4eadd3c7158c50b6bd08098a3c79d50c7c809e7e
LencoDigitexer <addminuser258@gmail.com>
2020-03-11 17:51:05 +0200

    Исключение перекодировки служебных файлов

--- scripts/decode_script.py
+++ scripts/decode_script.py
@@ -5,11 +5,16 @@ output = "tmp2.txt"
 lst = os.listdir(path=".")
 
 for i in range(0, len(lst)):
-    print(lst[i])
-    filename = lst[i]
-    output = lst[i]
+    try:
+        print(lst[i])
+        if lst[i] == "decode_script.py" or lst[i] == "decode_script.exe":
+            continue
+        filename = lst[i]
+        output = lst[i]
     
-    f = codecs.open(filename, 'r', 'utf-8')
-    u = f.read()   # now the contents have been transformed to a Unicode string
-    out = codecs.open(output, 'w', 'cp1251')
-    out.write(u)   # and now the contents have been output as UTF-8
+        f = codecs.open(filename, 'r', 'utf-8')
+        u = f.read()   # now the contents have been transformed to a Unicode string
+        out = codecs.open(output, 'w', 'cp1251')
+        out.write(u)   # and now the contents have been output as UTF-8
+    except:
+        print("Замечена папка или иной объект - пропускаем")
